d4fb1e3eed9549c644cd0dac43f5243db37b8bd7
VladislavRogozhin <vrogozhin@alfabank.ru>
2023-06-17 19:41:46 +0400

    add description

--- sprint3/a.js
+++ sprint3/a.js
@@ -1,3 +1,29 @@
+/*
+
+Problem Statement: Generating Parentheses
+
+Given an integer n, generate all possible valid parentheses sequences of length 2n. A valid parentheses sequence consists of opening and closing parentheses, and each opening parenthesis must have a corresponding closing parenthesis.
+
+Write a function that takes an integer n (0 ≤ n ≤ 10) as input and prints all possible valid parentheses sequences in lexicographical (alphabetical) order.
+
+Input:
+An integer n (0 ≤ n ≤ 10) representing the length of parentheses sequences.
+
+Output:
+Print all possible valid parentheses sequences of length 2n, each on a new line.
+
+Example:
+Input: 3
+Output:
+((()))
+(()())
+(())()
+()(())
+()()()
+
+*/
+
+
 const readline = require('readline');
 
 const reader = readline.createInterface({
